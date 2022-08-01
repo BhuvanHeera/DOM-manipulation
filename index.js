004a2ee9247f4327ba6f7771873b017e6ab2be45
BhuvanHeera <prakasambhuvanesh@gmail.com>
2022-08-01 21:05:01 +0530

    dom

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const taskContainer = document.querySelector(".task__container");
-console.log(taskContainer);
+
+const generateCard = (taskData)=>`
+<div class="col-md-6 col-lg-4"  id=${taskData.id}>
+                 <div class="card text-center">
+                     <div class="card-header d-flex justify-content-end gap-2">
+                       <button type="button" class="btn btn-outline-danger"><i class="fa-solid fa-pencil"></i></button>
+                       <button type="button" class="btn btn-outline-warning"><i class="fa-solid fa-trash-can"></i></button>
+                     </div>
+                     <img src=${taskData.imageUrl} class="card-img-top " alt="..."/>
+                     <div class="card-body">
+                       <h5 class="card-title">${taskData.taskTitle}</h5>
+                       <p class="card-text">${taskData.taskDescription}</p>
+                       <a href="#" class="btn btn-primary">${taskData.taskType}</a>
+                     </div>
+                     <div class="card-footer text-muted">
+                         <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+                     </div>
+                   </div>
+             </div>
+`;
 
 const saveChanges= () => {
     const taskData = {
@@ -9,24 +28,6 @@ const saveChanges= () => {
         taskType: document.getElementById("tasktype").value,
         taskDescription: document.getElementById("taskdescription").value,
     };
-   const newCard =`
-   <div class="col-md-6 col-lg-4"  id=${taskData.id}>
-                    <div class="card text-center">
-                        <div class="card-header d-flex justify-content-end gap-2">
-                          <button type="button" class="btn btn-outline-danger"><i class="fa-solid fa-pencil"></i></button>
-                          <button type="button" class="btn btn-outline-warning"><i class="fa-solid fa-trash-can"></i></button>
-                        </div>
-                        <img src=${taskData.imageUrl} class="card-img-top " alt="..."/>
-                        <div class="card-body">
-                          <h5 class="card-title">${taskData.taskTitle}</h5>
-                          <p class="card-text">${taskData.taskDescription}</p>
-                          <a href="#" class="btn btn-primary">${taskData.taskType}</a>
-                        </div>
-                        <div class="card-footer text-muted">
-                            <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
-                        </div>
-                      </div>
-                </div>
-   `;
-   taskContainer.insertAdjacentHTML("beforeend",newCard);
+   
+   taskContainer.insertAdjacentHTML("beforeend",generateCard(taskData));
 };
